d7ba383b6a4fac03ea1de9f09be81a6d6a2ef3d5
revanth7667@gmail.com <revanth7667@gmail.com>
2023-11-11 20:22:31 +0000

    Added Notebooks

---
+++ Notebooks/Preparation.py
@@ -0,0 +1,33 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC CREATE OR REPLACE TABLE
+# MAGIC   songs_prepared (
+# MAGIC     artist_id STRING,
+# MAGIC     artist_name STRING,
+# MAGIC     duration DOUBLE,
+# MAGIC     release STRING,
+# MAGIC     tempo DOUBLE,
+# MAGIC     time_signature DOUBLE,
+# MAGIC     title STRING,
+# MAGIC     year DOUBLE,
+# MAGIC     processed_time TIMESTAMP
+# MAGIC   );
+# MAGIC
+# MAGIC INSERT INTO
+# MAGIC   songs_prepared
+# MAGIC SELECT
+# MAGIC   artist_id,
+# MAGIC   artist_name,
+# MAGIC   duration,
+# MAGIC   release,
+# MAGIC   tempo,
+# MAGIC   time_signature,
+# MAGIC   title,
+# MAGIC   year,
+# MAGIC   current_timestamp()
+# MAGIC FROM
+# MAGIC   songs
+
+# COMMAND ----------
+
+
